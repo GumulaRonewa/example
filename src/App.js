38974b43f0441660ee6b681c8a1291a5bea87cf9
gmlron001 <gmlron001@myuct.ac.za>
2021-08-12 03:49:46 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import { withStyles } from "@material-ui/core/styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledButton = withStyles({
+  root: {
+    background: "#F93800",
+    borderRadius: 6,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+})(Button);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: null,
+
+      password: null,
+      error: false,
+    };
+  }
+  
+  handleClick = (e) => {
+    
+  };
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <div className="wraps">
+
+        <div className={"roots"} variant="outlined">
+          
+          <div className="loginemail">
+            <TextField
+              placeholder=""
+              label="Province"
+              name="Province"
+              margin="normal"
+            />
+          </div>
+          <div className="loginemail">
+            <TextField
+              placeholder=""
+              label="City"
+              name="City"
+              margin="normal"
+            />
+          </div>
+          <div className="loginemail">
+            <TextField
+              placeholder=""
+              label="from"
+              name="from"
+              margin="normal"
+            />
+          </div>
+          <div className="loginemail">
+            <TextField
+              placeholder=""
+              label="to"
+              name="to"
+              margin="normal"
+            />
+          </div>
+         
+
+ 
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
